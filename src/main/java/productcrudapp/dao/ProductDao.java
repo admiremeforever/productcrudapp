a479a826da338d256f6a0477cef819e92bf5e7b1
admiremeforever <poddarmanish1997@gmail.com>
2023-03-18 17:40:33 +0530

    producrcrudapp V1.00

---
+++ src/main/java/productcrudapp/dao/ProductDao.java
@@ -0,0 +1,43 @@
+package productcrudapp.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+import productcrudapp.model.Product;
+
+@Component
+public class ProductDao {
+
+	@Autowired
+	HibernateTemplate hibernateTemplate;
+
+	// create product
+	// Transectional is used where we need to make some update in db
+	@Transactional
+	public void createProduct(Product product) {
+		this.hibernateTemplate.saveOrUpdate(product);
+	}
+
+	// for getting all product
+	public List<Product> getProducts() {
+		List<Product> products = this.hibernateTemplate.loadAll(Product.class);
+		return products;
+	}
+
+	// delete single product
+	@Transactional
+	public void deleteProduct(int pid) {
+		Product p = this.hibernateTemplate.load(Product.class, pid);
+		this.hibernateTemplate.delete(p);
+	}
+
+	// get single product
+	public Product getProduct(int pid) {
+		return this.hibernateTemplate.get(Product.class, pid);
+	}
+}
